6be5048af86f517364a66a1c708f45c173160760
Devin Mullin <devin.mullin90@gmail.com>
2021-10-13 11:42:34 -0500

    commit tree eeb3ba0902d63776b2c10902597bfd1a01d57f67

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ['dev', 'code', 'fun', 'not']
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array = ['fun', 'cool'], element = "array!")
+  array.push(element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array = ['cool', 'fun'], element = 'wow')
+  array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(array = ['great', 'grand', 'wonderful', 'no yelling on the bus'])
+  array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(array = ['great', 'grand', 'wonderful', 'no yelling on the bus'])
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
